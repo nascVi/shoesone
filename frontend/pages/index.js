9d32184a04bcfe649bb4194926fbaa7f4b103511
SAHABANNISA MOHAMED <jmosah3@onjci.com>
2019-06-12 23:14:46 +0200

    initial commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,15 @@
+/* pages/index.js */
+import { Button, Alert } from "reactstrap";
+
+export default () => {  
+  return (
+    <div>
+      <div>
+        <Alert color="primary">
+          lpChamps is building shoesone972.com here and now 2019 Jun the 12th, 22:35
+        </Alert>
+        &nbsp; <Button color="primary">Hello from nextjs</Button>
+      </div>
+    </div>
+  );
+};
