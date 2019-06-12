26944b0872e357c204baf0dae0513477300ff13c
SAHABANNISA MOHAMED <jmosah3@onjci.com>
2019-06-12 23:29:43 +0200

    auths basics settings

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,15 +1,16 @@
-/* pages/index.js */
+/* /pages/index.js */
 import { Button, Alert } from "reactstrap";
+import Layout from "../components/Layout";
 
-export default () => {  
+export default () => {
   return (
-    <div>
+    <Layout>
       <div>
         <Alert color="primary">
-          lpChamps is building shoesone972.com here and now 2019 Jun the 12th, 22:35
+        lpChamps is building shoesone972.com here and now 2019 Jun the 12th, 22:35
         </Alert>
         &nbsp; <Button color="primary">Hello from nextjs</Button>
       </div>
-    </div>
+    </Layout>
   );
 };
