3590f3d7c690a6f8d6d3401ee44b76b3b90e825e
nascVi <vaiiho.nascimbene@gmail.com>
2019-11-21 07:18:22 +0100

    push sort

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React from 'react';
-<<<<<<< HEAD
 import logo from './shoes1.svg';
 import Footer from './components/Footer';
 import './App.css';
 
-=======
 import logo from './poppie.svg';
 import './App.css';
 
 import Footer from './components/Footer';
 
->>>>>>> a5109ccaa3d6fb4854d0eea91e48d9e5cf701388
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
         <h1>Benvenuti a Shoes 0 972</h1>
         <p>Non si piace per la venuti di scorson</p>
         <scan className="span">paziensa a la magnana per favor</scan>
         <a href="https://www.instagram.com/shoesone972/"><img src={logo} className="App-logo" alt="logo" /></a>
         <br/>
-=======
         <h1>Bienenuti a Shoes O 972</h1>        
         <a
         href="https://instagram.com/shoesoneuf972"
@@ -31,7 +26,6 @@ function App() {
         <p>
           Paziensa per la ultimate veg
         </p>
->>>>>>> a5109ccaa3d6fb4854d0eea91e48d9e5cf701388
         <Footer />
       </header>
     </div>
