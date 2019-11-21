bde689f9c43e184a7ab197a7d0f310730fa213c2
nascVi <vaiiho.nascimbene@gmail.com>
2019-11-21 07:21:39 +0100

    push build

---
+++ build/precache-manifest.b80032751d77704035677515d0f7cda9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32298c491acbb1ed9f0f3fe9bb2d7d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10113bf875724afc597f",
+    "url": "/static/css/main.50572cf8.chunk.css"
+  },
+  {
+    "revision": "57043f759ed4a699e421",
+    "url": "/static/js/2.ae6457a5.chunk.js"
+  },
+  {
+    "revision": "10113bf875724afc597f",
+    "url": "/static/js/main.fe62107d.chunk.js"
+  },
+  {
+    "revision": "0a136620b19f3f27330f",
+    "url": "/static/js/runtime-main.73f86e5c.js"
+  },
+  {
+    "revision": "fa1514e68585505d845e2c2a0d51a5ce",
+    "url": "/static/media/shoes1.fa1514e6.svg"
+  }
+]);
