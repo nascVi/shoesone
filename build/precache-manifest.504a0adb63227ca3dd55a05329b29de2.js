a5109ccaa3d6fb4854d0eea91e48d9e5cf701388
nascVi <vaiiho.nascimbene@gmail.com>
2019-11-21 05:38:51 +0100

    black branch level up

---
+++ build/precache-manifest.504a0adb63227ca3dd55a05329b29de2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b810ecafb6aea408928c1ed2d1c1980f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d4ad77d8243bb4a0e1a",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "af955a7f8a68cf3b3bd1",
+    "url": "/static/js/2.96a06041.chunk.js"
+  },
+  {
+    "revision": "6d4ad77d8243bb4a0e1a",
+    "url": "/static/js/main.b81a449f.chunk.js"
+  },
+  {
+    "revision": "0a136620b19f3f27330f",
+    "url": "/static/js/runtime-main.73f86e5c.js"
+  },
+  {
+    "revision": "84f2280ffa62a22539da1f0b1f3e630f",
+    "url": "/static/media/poppie.84f2280f.svg"
+  }
+]);
