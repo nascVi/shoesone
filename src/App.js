3b50d2f955d31743d74da54428054c95c4ba7d56
nascVi <vaiiho.nascimbene@gmail.com>
2019-12-03 10:25:42 +0100

    com push for owner

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <h1>Bienvenu chez Shoes 0 972</h1>
         <p>Nos produits sont accessibles sur <a href="https://www.instagram.com/shoesone972/">Instagram</a> pour l'instant.</p>
-        <span className="span">Pour la Martinique, nous livrons en main propre, dans les 72h.</span>
+        <span className="span">Pour la Martinique, nous livrons en main propre, dans les 72h(5€).</span>
         <a href="https://www.instagram.com/shoesone972/"><img src={logo} className="App-logo" alt="logo" /></a>
         <br/>
         <Footer />
