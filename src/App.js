6b6d14d3bb0b95e2d03d6f307101d8c9b72ad9e2
nascVi <vaiiho.nascimbene@gmail.com>
2019-12-10 16:11:02 +0100

    title index change req

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Bienvenu chez Shoes 0ne 972</h1>
+        <h1>Bienvenu chez Shoes0ne 972</h1>
         <p>Nos produits sont accessibles sur <a href="https://www.instagram.com/shoesone972/">Instagram</a> pour l'instant.</p>
         <span className="span">Pour la Martinique, nous livrons en main propre, dans les 72h(5€).</span>
         <a href="https://www.instagram.com/shoesone972/"><img src={logo} className="App-logo" alt="logo" /></a>
