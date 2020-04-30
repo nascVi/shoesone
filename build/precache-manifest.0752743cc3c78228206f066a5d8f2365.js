95254369f16aedccb58f34a0b6a8872de97fdc2f
nascVi <vaiiho.nascimbene@gmail.com>
2020-04-30 11:17:42 +0200

    ico test

---
+++ build/precache-manifest.0752743cc3c78228206f066a5d8f2365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "356e432d2eb32366c68c97d37a0f5bbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdbd16d853125b405299",
+    "url": "/static/css/main.4d831597.chunk.css"
+  },
+  {
+    "revision": "af955a7f8a68cf3b3bd1",
+    "url": "/static/js/2.96a06041.chunk.js"
+  },
+  {
+    "revision": "cdbd16d853125b405299",
+    "url": "/static/js/main.ed2e795f.chunk.js"
+  },
+  {
+    "revision": "0a136620b19f3f27330f",
+    "url": "/static/js/runtime-main.73f86e5c.js"
+  },
+  {
+    "revision": "562daf76d0354b46fb975eabca856afa",
+    "url": "/static/media/shoes1.562daf76.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f0a50aaaf000830f6ded5b561ac60f6.js"
+  "/precache-manifest.0752743cc3c78228206f066a5d8f2365.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
