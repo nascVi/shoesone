110fbd2234599ec432c3e3fed941f6953234385c
nascVi <vaiiho.nascimbene@gmail.com>
2020-04-30 11:30:39 +0200

    Traduction du titre Infos de l'originale

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Bienvenu chez Shoes0ne 972</h1>
-        <p>Nos produits sont accessibles sur <a href="https://www.instagram.com/shoesone972/">Instagram</a> pour l'instant.</p>
+        <h1>BEL BONJOU, BIENVENU sur Shoesone</h1>
+        <p>Nos produits sont aussi<a href="https://www.instagram.com/shoesone972/">Instagram</a> pour l'instant.</p>
         <span className="span">Pour la Martinique, nous livrons en main propre, dans les 72h(5€).</span>
         <a href="https://www.instagram.com/shoesone972/"><img src={logo} className="App-logo" alt="logo" /></a>
         <br />
