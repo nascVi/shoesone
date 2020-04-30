9a8581afaa7adb253fb81f6b848e8d5c8b62e776
nascVi <vaiiho.nascimbene@gmail.com>
2020-04-30 12:08:58 +0200

    boat color roaming static

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>BEL BONJOU, BIENVENU sur Shoesone <span className="dMartinique" role="img" aria-label="madinina drapeau">🛳</span>!</h1>
+        <h1>BEL BONJOU, BIENVENU sur Shoesone <span className="dMartinique" role="img" aria-label="madinina drapeau">🛳</span> !</h1>
         <p>Nos produits sont aussi <a className="Imlink" href="https://www.instagram.com/shoesone972/">Instagram</a> pour l'instant.</p>
         <span className="span">Pour la Martinique, nous livrons en main propre, dans les 72h(5€).</span>
         <a href="https://www.instagram.com/shoesone972/"><img src={logo} className="App-logo" alt="logo" /></a>
