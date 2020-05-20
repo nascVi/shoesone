0597cd9d10aab85ae6db4622edbb1d8bd94b3e33
nascvi <vaiiho.nascimbene@gmail.com>
2020-05-20 11:38:10 +0200

    Apollo scopes 🔭

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const session = require('koa-session');
 
 dotenv.config();
 const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
+const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -25,7 +26,7 @@ app.prepare().then(() => {
     createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
-      scopes: ['read_products'],
+      scopes: ['read_products', 'write_products'],
       afterAuth(ctx) {
         const { shop, accessToken } = ctx.session;
         ctx.cookies.set('shopOrigin', shop, {
@@ -38,7 +39,7 @@ app.prepare().then(() => {
     }),
   );
 
-  server.use(graphQLProxy());
+  server.use(graphQLProxy({version: ApiVersion.October19}))
   server.use(verifyRequest());
   server.use(async (ctx) => {
     await handle(ctx.req, ctx.res);
