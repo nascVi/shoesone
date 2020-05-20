c0e5c688c0e9843ccf3d74190799dd22cc207e9d
nascvi <vaiiho.nascimbene@gmail.com>
2020-05-20 10:05:30 +0200

    initial for shopify 🛍'

---
+++ .next/static/chunks/styles.2f4fd886a5f7241308ff.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{"kdv+":function(n,w,o){}}]);
---
+++ .next/static/chunks/styles.b2b280d54b92e71676d5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{"kdv+":function(n,w,o){}}]);
