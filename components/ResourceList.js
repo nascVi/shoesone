5202c933cc14ffa9a1334786c234081492001a50
nascvi <vaiiho.nascimbene@gmail.com>
2020-05-20 15:00:38 +0200

    Products Customization 🍮

--- components/ResourceList.js
+++ components/ResourceList.js
@@ -8,6 +8,8 @@ import {
     Thumbnail,
 } from '@shopify/polaris';
 import store from 'store-js';
+import { Redirect } from '@shopify/app-bridge/actions';
+import { Context } from '@shopify/app-bridge-react';
 
 const GET_PRODUCTS_BY_ID = gql`
   query getProducts($ids: [ID!]!) {
@@ -39,7 +41,19 @@ const GET_PRODUCTS_BY_ID = gql`
 `;
 
 class ResourceListWithProducts extends React.Component {
+
+    static contextType = Context;
+
     render() {
+      const app = this.context;
+      const redirectToProduct = () => {
+          const redirect = Redirect.create(app);
+          redirect.dispatch(
+          Redirect.Action.APP,
+          '/edit-products',
+          );
+      };
+        
       const twoWeeksFromNow = new Date(Date.now() + 12096e5).toDateString();
       return (
         <Query query={GET_PRODUCTS_BY_ID} variables={{ ids: store.get('ids') }}>
@@ -75,6 +89,10 @@ class ResourceListWithProducts extends React.Component {
                       id={item.id}
                       media={media}
                       accessibilityLabel={`View details for ${item.title}`}
+                      onClick={() => {
+                        store.set('item', item);
+                        redirectToProduct();
+                      }}
                     >
                       <Stack>
                         <Stack.Item fill>
