e2ae68920a8e2d50bea6be6a9d20fba0b62d1420
nascvi <vaiiho.nascimbene@gmail.com>
2020-05-20 14:33:26 +0200

    Products Display 🎁

--- components/ResourceList.js
+++ components/ResourceList.js
@@ -1,6 +1,13 @@
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
-import { Card } from '@shopify/polaris';
+import { 
+    Card,
+    ResourceList,
+    Stack,
+    TextStyle,
+    Thumbnail,
+} from '@shopify/polaris';
+import store from 'store-js';
 
 const GET_PRODUCTS_BY_ID = gql`
   query getProducts($ids: [ID!]!) {
@@ -33,15 +40,61 @@ const GET_PRODUCTS_BY_ID = gql`
 
 class ResourceListWithProducts extends React.Component {
     render() {
+      const twoWeeksFromNow = new Date(Date.now() + 12096e5).toDateString();
       return (
-        <Query query={GET_PRODUCTS_BY_ID}>
+        <Query query={GET_PRODUCTS_BY_ID} variables={{ ids: store.get('ids') }}>
           {({ data, loading, error }) => {
             if (loading) return <div>Loading…</div>;
             if (error) return <div>{error.message}</div>;
             console.log(data);
             return (
               <Card>
-                <p>stuff here</p>
+                <p>No sneakers & cleanings</p>
+                <ResourceList
+                showHeader
+                resourceName={{ singular: 'Product', plural: 'Products' }}
+                items={data.nodes}
+                renderItem={item => {
+                  const media = (
+                    <Thumbnail
+                      source={
+                        item.images.edges[0]
+                          ? item.images.edges[0].node.originalSrc
+                          : ''
+                      }
+                      alt={
+                        item.images.edges[0]
+                          ? item.images.edges[0].node.altText
+                          : ''
+                      }
+                    />
+                  );
+                  const price = item.variants.edges[0].node.price;
+                  return (
+                    <ResourceList.Item
+                      id={item.id}
+                      media={media}
+                      accessibilityLabel={`View details for ${item.title}`}
+                    >
+                      <Stack>
+                        <Stack.Item fill>
+                          <h3>
+                            <TextStyle variation="strong">
+                              {item.title}
+                            </TextStyle>
+                          </h3>
+                        </Stack.Item>
+                        <Stack.Item>
+                          <p>${price}</p>
+                        </Stack.Item>
+                        <Stack.Item>
+                          <p>Expires on {twoWeeksFromNow} </p>
+                        </Stack.Item>
+                      </Stack>
+                    </ResourceList.Item>
+                  );
+                }}
+              />
               </Card>
             );
           }}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import { EmptyState, Layout, Page } from '@shopify/polaris';
 import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
+import store from 'store-js';
+
+import ResourceListWithProducts from '../components/ResourceList';
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 class Index extends React.Component {
   state = { open: false };
+
   render() {
+    const emptyState = !store.get('ids');
     return (
       <Page>
         <TitleBar
@@ -21,19 +26,23 @@ class Index extends React.Component {
           onSelection={(resources) => this.handleSelection(resources)}
           onCancel={() => this.setState({ open: false })}
         />
-        <Layout>
-          <p>Shoesone 972 shopify version</p>
-        <EmptyState
-          heading="Discount your products temporarily"
-          action={{
-            content: 'Select products',
-            onAction: () => this.setState({ open: true }),
-          }}
-          image={img}
-          >
-            <p>Select products to change their price temporarily.</p>
-          </EmptyState>
-        </Layout>
+        {emptyState ? (
+          <Layout>
+            <p>Shoesone 972 shopify version</p>
+          <EmptyState
+            heading="Discount your products temporarily"
+            action={{
+              content: 'Select products',
+              onAction: () => this.setState({ open: true }),
+            }}
+            image={img}
+            >
+              <p>Select products to change their price temporarily.</p>
+            </EmptyState>
+          </Layout>
+        ) : (
+          <ResourceListWithProducts />
+        )}
       </Page>
     )
   }
@@ -41,6 +50,7 @@ class Index extends React.Component {
     const idsFromResources = resources.selection.map((product) => product.id);
     this.setState({ open: false });
     console.log(idsFromResources)
+    store.set('ids', idsFromResources);
   };
 }
 
