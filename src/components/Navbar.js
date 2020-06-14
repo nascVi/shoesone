01de07bb37033ea84701be546b96174e05b33eae
nascVi <vaiiho.nascimbene@gmail.com>
2020-06-14 07:04:23 +0200

    cart and  signup button 🔲

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { FaAlignRight } from "react-icons/fa";
+// import { FaAlignRight } from "react-icons/fa";
+import { FaShoppingBag } from "react-icons/fa"
 import logo from "../images/logo.svg";
+
+import HamburgerElastic from './thisam/HamburgerElastic'
 
 export default class Navbar extends Component {
   state = {
@@ -10,21 +13,39 @@ export default class Navbar extends Component {
   handleToggle = () => {
     this.setState({ isOpen: !this.state.isOpen });
   };
+
+  // state = {
+  //   showSideBar: false,
+  // }
+
+  // showSideBar = () => {
+  //   const { showSideBar } = this.state;
+  //   this.setState({ showSideBar: !showSideBar });
+  // }
+
   render() {
     return (
       <nav className="navbar">
         <div className="nav-center">
           <div className="nav-header">
+            <HamburgerElastic
+            id="button" 
+            onClick={this.handleToggle}
+            isActive={this.state.isOpen} 
+            barColor="#af9a7d" buttonWidth={16} 
+            toggleButton={this.isActive} 
+            className="nav-btn nav-icon" />
             <Link className="nav-logoLink" to="/">
-              <img src={logo} alt="Shoesone" />
+              <img src={logo} height='35' width='135' alt="Shoesone" />
             </Link>
-            <button
+            {/* <button
               type="button"
-              className="nav-btn"
-              onClick={this.handleToggle}
+              className=""
+              onClick=
             >
               <FaAlignRight className="nav-icon" />
-            </button>
+            </button> */}
+            <FaShoppingBag className="cart cart-icon snipcart-checkout snipcart-overwrite" />
           </div>
           <ul
             className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
@@ -40,6 +61,13 @@ export default class Navbar extends Component {
             </li>
             <li>
               <Link to="/custom">Custom</Link>
+            </li>
+            <li>
+              <Link
+                className="loginbtn btn snipcart-customer-signin text-capitalize"
+                >
+                  SignUp/In
+              </Link>
             </li>
           </ul>
         </div>
