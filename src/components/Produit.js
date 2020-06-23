1a12735cfb04e2b9df45cfc24ab603a7391875b5
nascvi <vaiiho.nascimbene@gmail.com>
2020-06-23 17:05:19 +0200

    discount display polished 💅

--- src/components/Produit.js
+++ src/components/Produit.js
@@ -4,16 +4,18 @@ import defaultImg from "../images/requinsonshore.jpeg";
 import PropTypes from "prop-types";
 import { memo } from "react";
 
-const Produit = memo(({ produit }) => {
-  const { name, slug, images, price } = produit;
+const Produit = memo(({ produit}) => {
+  const { name, slug, images, price, comprice} = produit;
   // console.log(name);
   return (
     <article className="produit">
       <div className="img-container">
         <img src={images[0] || defaultImg} alt="le produit" />
         <div className="price-top">
-          <h6>€{price}</h6>
-          {/* <p className="produit-discount">Soldé à 40%: <strike>${price + 24}</strike></p> */}
+          <h6>{price}€</h6>
+            <p className="produit-discount">
+            {comprice === 0 ?  `` : `Soldé de l'initiale ${comprice}€`}
+            </p>
         </div>
         <Link to={`/produits/${slug}`} className="btn-primary produit-link">
           description du produit
@@ -29,7 +31,8 @@ Produit.propTypes = {
     name: PropTypes.string.isRequired,
     slug: PropTypes.string.isRequired,
     images: PropTypes.arrayOf(PropTypes.string).isRequired,
-    price: PropTypes.number.isRequired
+    price: PropTypes.number.isRequired,
+    comprice: PropTypes.number.isRequired
   })
 };
 export default Produit;
